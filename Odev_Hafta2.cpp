91828bc65c364db1cce7588f475d4cbacfb9b5e0
Recep <103540193+recepoztekin@users.noreply.github.com>
2025-03-02 06:36:53 +0300

    Add files via upload

---
+++ Odev_Hafta2.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	/*int tam_sayi;
+	double ondalikli_sayi;
+
+	cout << "Bir tamsayý girin: ";
+	cin >> tam_sayi;
+	cout << "Bir ondalýklý sayý girin: ";
+	cin >> ondalikli_sayi;
+	cout << "\n";
+
+	cout << "Sonuçlar: " << endl;
+	cout << tam_sayi << " + " << ondalikli_sayi << " = " << tam_sayi + ondalikli_sayi << endl;
+	cout << tam_sayi << " - " << ondalikli_sayi << " = " << tam_sayi - ondalikli_sayi << endl;
+	cout << tam_sayi << " * " << ondalikli_sayi << " = " << tam_sayi * ondalikli_sayi << endl;
+	cout << tam_sayi << " / " << ondalikli_sayi << " = " << tam_sayi / ondalikli_sayi << endl;
+	cout << tam_sayi << " % " << int(ondalikli_sayi) << " = " << tam_sayi % int(ondalikli_sayi) << "\n\n";
+
+	cout << "Veri Boyutlarý:\n";
+	cout << "int veri tipi: " << sizeof(int) << " byte alan kaplar.\n";
+	cout << "double veri tipi: " << sizeof(double) << " byte alan kaplar.";*/
+
+	//----------------------------------------------------------------------                            
+
+	/*string ad, soyad, meslek;
+
+	cout << "Adinizi Girin: ";
+	cin >> ad;
+	cout << "Soyadinizi Girin: ";
+	cin >> soyad;
+	cin.ignore();
+	cout << "Mesleginizi Girin: ";
+	getline(cin, meslek);
+	cout << "\n";
+
+	cout << "********************************\n";
+	cout << "*  Kiþi Bilgileri" << setw(16) << "*\n";
+	cout << "********************************\n";
+	cout << "* Ad: " << left << setw(25) << ad << "*\n";
+	cout << "* Soyad: " << left << setw(22) << soyad << "*\n";
+	cout << "* Meslek: " << setw(21) << meslek << "*\n";
+	cout << "* Ad-Soyad Uzunluk: " << setw(11) << ad.length() + soyad.length() << "*\n";
+	cout << "********************************\n";
+
+	cout << "Merhaba, ben " << ad.append(soyad) << " " << meslek << " olarak calisiyorum.";*/
+
+	//----------------------------------------------------------------------
+
+
+	//int a = 5, b = 7; // a ve b deðiþkenlerine 5 ve 7 deðerleri atanmýþtýr.
+	//double c = 3.5; // c deðiþkenine 3.5 deðeri atanmýþtýr.
+	//   
+	//cout << "a == b: " << (a = b) << endl; // Atama iþlemi olduðu için sonuç 7 olacaktýr.
+	//   cout << "a > c: " << a > c << endl; // a ve c karþýlaþtýrýlamaz çünkü a int veri türünde c ise double veri türündedir.
+	//cout << "b <= c: " << b <= c << endl;// b ve c karþýlaþtýrýlamaz çünkü b int veri türünde c ise double veri türündedir.
+	//   
+	//bool kosul1 = (a > b) && (b < c);// a > b koþulu yanlýþ olduðu için ve operatörü ile baðlý olan b < c koþulu kontrol edilmeyecektir ve sonuc false dönecektir.
+	//bool kosul2 = (a == 7) || (c = 3.5);// kod hata verir çünkü c = 3.5 atama iþlemi yapýlýrken == operatörü kullanýlmalýdýr.
+	//   
+	//   cout << "Koþul 1: " << kosul1 << endl; // false
+	//cout << "Koþul 2: " << kosul2 << endl; // hata verir çünkü yukarýda yanlýþ operatör kullanýldý.
+	//   
+	//if (kosul1 && kosul2)
+	//	cout << "Her iki koþul da doðru!" // ; eklenmeli
+	//else if (kosul1 || kosul2)
+	//	cout << "En az bir koþul doðru!" // ; eklenmeli
+	//else
+	//	cout << "Hiçbir koþul doðru deðil!" // ; eklenmeli
+	
+
+	//---------------------------------------------------------------------
+
+	/*int baslangic;
+
+	cout << "Baslangic Degeri Girin: ";
+	cin >> baslangic;
+	cout << "\n\n";
+
+	cout << "Adim 1: " << baslangic << " -> ";
+	baslangic = baslangic + 5;
+	cout << baslangic << " (5 artirildi)\n";
+
+	cout << "Adim 2: " << baslangic << " -> ";
+	baslangic *= 2;
+	cout << baslangic << " (2 ile carpildi.)\n";
+
+	cout << "Adim 3: " << baslangic << " -> ";
+	baslangic = baslangic - 3;
+	cout << baslangic << " (3 azaltildi.)\n";
+
+	cout << "Adim 4: " << baslangic << " -> ";
+	baslangic %= 10;
+	cout << baslangic << " (10 ile bolumunden kalan.)\n";
+
+	bool sonuc = (baslangic > 3 && baslangic < 7) || (baslangic == 0);
+	cout << "Adim 5: " << baslangic << " degeri, 3'ten buyuk ve 7'den kucuk VEYA 0'a esit mi? Sonuc: " << sonuc;
+	if (sonuc == 0)
+	{
+		cout << " (Yanlis)";
+	}
+	else
+	{
+		cout << " (Dogru)";
+	}
+
+	return 0;*/
+}
+
+
